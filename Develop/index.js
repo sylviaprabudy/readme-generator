8968e3b930f1150221c17d38300fc57440941e1b
Sylvia <sylviaprabudy@gmail.com>
2020-07-19 13:41:11 -0500

    add questions and dependencies

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,7 @@
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMardown");
 
 // array of questions for user
 const questions = [
@@ -7,17 +10,27 @@ const questions = [
         type: "input",
         name: "github",
         message: "What's your Github username?"
-    }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's your email?"
+    },
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    return.fs.writeFile(path.join(process.cwd(), fileName), data);
+    return fs.writeFile(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions)
+    .then((inquirerAnswers) => {
+        console.log("Generating.... Please wait....");
+        writeToFile("README.md", generateMarkdown({ ...inquirerAnswers }));
+    })
 
 }
 
