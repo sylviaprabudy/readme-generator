0137c1f753b74b655c8822403b4f7ed209fc8306
Sylvia <sylviaprabudy@gmail.com>
2020-07-19 13:28:54 -0500

    install npm and npm inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,19 @@
+const inquirer = require("inquirer")
+
 // array of questions for user
 const questions = [
+
+    {
+        type: "input",
+        name: "github",
+        message: "What's your Github username?"
+    }
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return.fs.writeFile(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
